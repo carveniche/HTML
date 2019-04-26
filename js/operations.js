2350151eba8aae527777ee859a9399dae289d637
nups89 <41606354+nups89@users.noreply.github.com>
2019-04-26 15:48:00 +0530

    Add files via upload

--- js/operations.js
+++ js/operations.js
@@ -18,18 +18,55 @@ app.controller("myCtrl", function($scope) {
     $scope.multiplicationType = ['Select','Vertical','Horizontal'];
     $scope.symbols = [{value:'&copy'},{value:'+'},{value:'='}];
     $scope.selectedColForSymbol = {};
+    $scope.solutionVisibility = false;
     $scope.questionMetaData = {
         choiceTypeSelected:"Select",
         qsTypeSelected:"Select",
         multiplicationType:"Select",
-        title:""
+        title:"",
+        solutionType:""
     };
+    $scope.solutionType = ['Model','Side By Side'];
+    $scope.solutionContent = [];
+    $scope.solutionCols = [];
+    $scope.solutionRows = [];
+    $scope.populateChoiceArray = function(){
+        var choiceSelected = $scope.questionMetaData.choiceTypeSelected;
+        if(choiceSelected == 'Multi Select'){
+            if($scope.choices.length==0){
+                alert('Please add atleast one choice');
+                return;
+            }
+        };
+        if(choiceSelected == 'Drag And Drop'){
+            for(var i=0;i<$scope.ritems.length;i++){
+                var colArray = $scope.ritems[i].cols;
+                for(var x=0;x<colArray.length;x++){
+                    if(colArray[x].checked == true){
+                        var found = $scope.choices.find(function(element) {
+                            return element.choice == colArray[x].value;
+                        });
+                        if(!found){
+                            var isValueEmpty = $scope.choices.find(function(element) {
+                                return element.choice == '';
+                            });
+                            if(isValueEmpty){
+                                isValueEmpty.choice =colArray[x].value;
+                            }else{
+                                $scope.choices.push({choice:colArray[x].value,answer:''});
+                            }
 
+                        }
+                    }
+                }
+            }
 
-    $scope.getColumnData=function(){
+        };
+    };
+    $scope.getColumnData=function(colData,rowData){
         var colArray = [];
-        for(var i=0;i<$scope.citems.length;i++){
-            colArray.push({col1:i+"-"+$scope.ritems.length,value:'',checked:false});
+        for(var i=0;i<colData.length;i++){
+            colArray.push({col1:i+"-"+rowData.length,value:'',checked:false});
 
         }
         return colArray;
@@ -41,16 +78,14 @@ app.controller("myCtrl", function($scope) {
         }
         $scope.ritems.push({row1:$scope.ritems.length,
             hint:"",
-            cols:$scope.getColumnData()});
+            cols:$scope.getColumnData($scope.citems,$scope.ritems)});
 
     };
     $scope.addColumn = function(){
         $scope.citems.push({col1:$scope.citems.length+"-"+$scope.ritems.length,value:'',checked:false});
         for(var i=0;i<$scope.ritems.length;i++){
             $scope.ritems[i].cols.push({col1:$scope.citems.length+"-"+$scope.ritems[i].row1,value:'',checked:false})
-        }
-
-
+        };
     }
     $scope.addChoice = function(){
         $scope.choices.push({choice:'',answer:'',index:999});
@@ -62,11 +97,11 @@ app.controller("myCtrl", function($scope) {
         var col = found.cols.find(function(ele){return ele.col1==idx.col1});
         col.checked=col.checked==true?false:true;
     };
-    $scope.removeRow = function(obj){
-        var found = $scope.ritems.findIndex(function(element) {
+    $scope.removeRow = function(obj,dataArray){
+        var found = dataArray.findIndex(function(element) {
             return element.row1 == obj.row1;
         });
-        $scope.ritems.splice(found,1);
+        dataArray.splice(found,1);
     };
     $scope.choiceVisibility = function(){
         var choiceSelected = $scope.questionMetaData.choiceTypeSelected;
@@ -76,29 +111,7 @@ app.controller("myCtrl", function($scope) {
         return true;
     };
     $scope.validationCheck = function(){
-        var choiceSelected = $scope.questionMetaData.choiceTypeSelected;
-        if(choiceSelected == 'Multi Select'){
-            if($scope.choices.length==0){
-                alert('Please add atleast one choice');
-                return;
-            }
-        };
-
-        if(choiceSelected == 'Drag And Drop'){
-            for(var i=0;i<$scope.ritems.length;i++){
-                var colArray = $scope.ritems[i].cols;
-                for(var x=0;x<colArray.length;x++){
-                    if(colArray[x].checked == true){
-                        var found = $scope.choices.find(function(element) {
-                            return element.choice == colArray[x].value;
-                        });
-                        if(!found)
-                        $scope.choices.push({choice:colArray[x].value,answer:''});
-                    }
-                }
-            }
-
-        };
+        $scope.populateChoiceArray();
         $('#exampleModal').modal('show');
     };
     $scope.limitKeypress = function($event,value){
@@ -109,8 +122,25 @@ app.controller("myCtrl", function($scope) {
         }
     };
     $scope.addSolution = function(){
-
+        $scope.solutionVisibility = true;
     };
+    $scope.addSolutionLine = function(){
+        $scope.solutionContent.push({value:""});
+    };
+    $scope.addSolutionRow = function(){
+        if($scope.solutionCols.length == 0){
+            $scope.solutionCols.push({col1:$scope.solutionCols.length+"-"+$scope.solutionRows.length,value:''});
+
+        }
+        $scope.solutionRows.push({row1:$scope.solutionRows.length,
+            cols:$scope.getColumnData($scope.solutionCols,$scope.solutionRows)});
+    };
+    $scope.addSolutionCol = function(){
+        $scope.solutionCols.push({col1:$scope.solutionCols.length+"-"+$scope.solutionRows.length,value:''});
+        for(var i=0;i<$scope.solutionRows.length;i++){
+            $scope.solutionRows[i].cols.push({col1:$scope.solutionCols.length+"-"+$scope.solutionRows[i].row1,value:'',})
+        };
+    }
     $scope.showSymbolModalPopup = function(col){
         $scope.selectedColForSymbol = col;
         $('#symbolModal').modal('show');
@@ -125,10 +155,44 @@ app.controller("myCtrl", function($scope) {
     $scope.insertImg = function(url){
         var x = document.createElement("IMG");
         x.setAttribute("src", url);
-        x.setAttribute("width", "40");
-        x.setAttribute("height", "40");
-        document.getElementById("testing").appendChild(x);
+        document.getElementById("questionTitle").appendChild(x);
         $('#iconModal').modal('hide');
+    };
+
+
+    $scope.onSubmit = function(){
+        $scope.populateChoiceArray();
+        var dataObj = {
+            "operation":$scope.questionMetaData.qsTypeSelected,
+            "additionTypes":$scope.questionMetaData.multiplicationType,
+            "questionName":$scope.questionMetaData.title,
+            "questionContent":[],
+            "choices":[],
+            "choiceType":$scope.questionMetaData.choiceTypeSelected,
+            "choiceCount":""};
+        for(var x=0;x<$scope.ritems.length;x++){
+            for(var y=0;y<$scope.ritems[x].cols.length;y++){
+                dataObj.questionContent.push({
+                    "row":x+1,
+                    "col":y+1,
+                    "value":$scope.ritems[x].cols[y].value,
+                    "isMissed":$scope.ritems[x].cols[y].checked
+                })
+            };
+
+        };
+        for(var i=0;i<$scope.choices.length;i++){
+            dataObj.choices.push($scope.choices[i].choice);
+        };
+        dataObj.choiceCount = dataObj.choices.length;
+    };
+    $scope.getDataTableStyle = function(){
+        if($scope.questionMetaData.solutionType == 'Side By Side'){
+            return{width:'30%',float:'left'}
+        }else{
+            return{width:'100%'}
+        }
+
     }
 
 })
